e59deb60758e24df489fc4424c1b7321dc357433
Davidson <dehpacifico@gmail.com>
2025-01-26 22:32:53 -0300

    conectando typeorm na aplicacao e mapendo entidade

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Response } from "express";
 import router from './index.js';
+import { AppDataSource } from "./config/dataSource.js";
+import 'reflect-metadata';
 
 const app = express();
 app.use(express.json());
 router(app);
+AppDataSource.initialize().then(() => {
+    console.log("Conectado ao banco sem erros.")}).catch((err) => {
+        console.error(err, "Erro na inicialização do banco");
+})
  
 export default app;
  
